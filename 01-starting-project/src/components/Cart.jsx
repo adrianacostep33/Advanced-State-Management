150b16d8b34ead21f81bcf6e4010b2f539fd9685
adrianacostep33 <adriana.coste@principal33.com>
2024-03-15 15:31:44 +0200

    Add card context to the entire app

--- 01-starting-project/src/components/Cart.jsx
+++ 01-starting-project/src/components/Cart.jsx
@@ -1,8 +1,8 @@
 import { useContext } from "react";
 import { CardContext } from "../store/shopping-cart-context";
 
-export default function Cart({ onUpdateItemQuantity }) {
-  const { items } = useContext(CardContext);
+export default function Cart() {
+  const { items, updateItemQuantity } = useContext(CardContext);
 
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
@@ -25,11 +25,11 @@ export default function Cart({ onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => updateItemQuantity(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => updateItemQuantity(item.id, 1)}>
                     +
                   </button>
                 </div>
